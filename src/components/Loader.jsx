d3310d22c5ef457f9a1fe5b03e6913e77b5534a0
= <talvaniyar@gmail.com>
2025-02-28 15:23:31 +0530

    second commit

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -3,7 +3,7 @@ import { FourSquare } from 'react-loading-indicators';
 
 const Loader = () => {
   return (
-    <div className='flex justify-center items-center h-screen w-screen'>
+    <div className='flex justify-center items-center h-screen w-screen bg-secondary'>
       <FourSquare color='#4f46e5' size='large' />
       <span className='ml-1 text-3xl font-bold text-primary'>vizTile</span>
     </div>
